a2ff7cebc4ad7df54a659aa19637ed2a6081085d
ratnamala <yadav.ratnamala95@gmail.com>
2020-05-06 12:04:37 +0530

    commit#5

--- src/Components/App2.js
+++ src/Components/App2.js
@@ -14,7 +14,9 @@ class App2 extends React.Component{
     const wordDisplay = this.state.isloggedIn?'in':'out'
     return(
       <div>
-        <h1>You're currently logged {wordDisplay}</h1>
+        {/* <h1>You're currently logged {wordDisplay}</h1> */}
+        <img src='https://www.fillmurray.com/200/100' onMouseOver={()=> alert('mouse over!')}/>
+        <button onClick={() =>console.log('clicked!')}>Click Me</button>
       </div>
     )
   }
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -4,7 +4,7 @@ import '../css/style.css'
 function TodoItem(props){
   return(
     <div className='todo-item'>
-      <input type='checkbox' checked={props.item.completed}  /><p>{props.item.text}</p>
+      <input type='checkbox' checked={props.item.completed} onChange={()=> console.log('changed')} /><p>{props.item.text}</p>
     </div>
   )
 }
